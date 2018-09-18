9e3785120fb68f968876ec8d352cbc17cf37aa9f
Kalen Janmohamed <kalen.janmohamed@Y22-Design-Kalen-Janmohamed.local>
2018-09-18 15:26:36 -0400

    Say Something GUI

---
+++ SaySomethingGUI04.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+
+#The first thing you have to do is create your main window.I always call this root.
+
+
+root = tk.Tk() #This calls the constructor Tk() snf builds your window
+
+
+
+
+
+
+
+
+root.mainloop() #This displays your root window and causes the program to wait. This is EVENT DRIVIEN PROGRAMMING
+
+print("END PROGRAM")
