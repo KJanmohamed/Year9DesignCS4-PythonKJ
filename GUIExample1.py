eabb808aab13f2f825074f3f7f09a160c44dbb0d
Kalen Janmohamed <kalen.janmohamed@Leafs0527.local>
2018-11-26 09:56:39 -0500

    GUI

--- GUIExample1.py
+++ GUIExample1.py
@@ -16,4 +16,19 @@ RB3.grid(row = 5, column = 0)
 RB4 = tk.Radiobutton(root, text="Player 4", variable=v, value=4)
 RB4.grid(row = 6, column = 0)
 
+Lab1 = tk.Label(root, text = "PLAYERS NAME")
+Lab1.grid(row = 2, column = 1, columnspan = 2)
+
+Txt1 = tk.Label(root, text = "Please Input your time for this Week", height = 10, width = 50)
+Txt1.grid(row = 3, column = 1, columnspan = 2, rowspan = 2)
+
+Ent1 = tk.Entry(root)
+Ent1.grid(row=5, column=1, columnspan = 2)
+
+Btn1 = tk.Button(root, text = "SUBMIT")
+Btn1.grid(row = 6, column=1, columnspan = 2)
+
+Txt2 = tk.Label(root, text = "Your average until week 4 is ")
+Txt2.grid(row = 7, column = 0, columnspan = 3)
+
 root.mainloop()
